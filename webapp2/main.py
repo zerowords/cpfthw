01b294705f6bdeba2fe8b28719f5a193d49153a1
fRui Apps <contact@fruiapps.com>
2012-04-28 01:27:02 +0530

    Cleaned the code

--- webapp2/main.py
+++ webapp2/main.py
@@ -1,12 +1,7 @@
-import cgi
 import webapp2
 from views import MainPage, CreateNote, DeleteNote, EditNote
 
-app = webapp2.WSGIApplication([
-        ('/', MainPage),
-        ('/create', CreateNote),
-        ('/edit/([\d]+)', EditNote),
-        ('/delete/([\d]+)', DeleteNote)
-        ], debug=True)
-
-
+app = webapp2.WSGIApplication([('/', MainPage), ('/create',
+                              CreateNote), ('/edit/([\d]+)', EditNote),
+                              ('/delete/([\d]+)', DeleteNote)],
+                              debug=True)
--- webapp2/models.py
+++ webapp2/models.py
@@ -1,9 +1,10 @@
 from google.appengine.ext import db
 
-class Notes(db.Model):
-  author = db.StringProperty()
-  text = db.StringProperty(multiline=True)
-  priority = db.StringProperty()
-  status = db.StringProperty()
-  date = db.DateTimeProperty(auto_now_add=True)
 
+class Notes(db.Model):
+
+    author = db.StringProperty()
+    text = db.StringProperty(multiline=True)
+    priority = db.StringProperty()
+    status = db.StringProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
